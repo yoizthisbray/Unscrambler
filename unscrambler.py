847b507cb9efce0c2ec8b490d8c059284e36fdee
yoizthisbray <68671752+yoizthisbray@users.noreply.github.com>
2020-08-21 20:55:03 -0400

    Add files via upload

---
+++ unscrambler.py
@@ -0,0 +1,14 @@
+def unscrambler(scrambled):
+    un_list = []
+    for i in scrambled:
+        if i not in "!@#$%^&*=,()":
+            un_list.append(i)
+    unscrambled = ""
+    print(unscrambled.join(un_list))
+
+
+def unit():
+    unscrambler(input("Scrambled text: "))
+
+
+unit()
